80a1fc3cc2b5d16b4dd06ffee03648f0ea4eb8e2
RasheedaIndusara <rasheedadusarai590@gmail.com>
2026-05-12 20:02:49 +0530

    Day01 Project just basics

---
+++ BCDDay1practice/src/main/java/lk/rasheeda/com/controller/UserServlet.java
@@ -0,0 +1,31 @@
+package lk.rasheeda.com.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lk.rasheeda.com.model.User;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/submit")
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String mobile = req.getParameter("mobile");
+
+        User user = new User();
+        user.setName(name);
+        user.setMobile(mobile);
+
+        resp.setContentType("text/plain");
+        PrintWriter out  = resp.getWriter();
+        out.println("Name: " + user.getName());
+        out.println("Mobile: " + user.getMobile());
+
+    }
+}
