95353b301d846fc0fb7f39743ec9d33e7e3fb70a
RasheedaIdusara <rasheedadusarai590@gmail.com>
2026-07-22 11:20:47 +0530

    Transaction manage using bmt

--- J2EE-Transactions/src/main/java/lk/rasheeda/jta/ejb/UserBeanimpl.java
+++ J2EE-Transactions/src/main/java/lk/rasheeda/jta/ejb/UserBeanimpl.java
@@ -1,17 +1,25 @@
 package lk.rasheeda.jta.ejb;
 
+import jakarta.annotation.Resource;
 import jakarta.ejb.Stateless;
+import jakarta.ejb.TransactionManagement;
+import jakarta.ejb.TransactionManagementType;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.UserTransaction;
 import lk.rasheeda.jta.entity.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class UserBeanimpl implements UserBean {
 
     @PersistenceContext(unitName = "JTA-PU")
     private EntityManager em;
+
+    @Resource
+    private UserTransaction utx;
 
     @Override
     public boolean login(String username, String password) {
@@ -21,19 +29,29 @@ public class UserBeanimpl implements UserBean {
     @Override
     public boolean register(String email, String name, String password) {
 
-        Session session = em.unwrap(Session.class);
+       // Session session = em.unwrap(Session.class);
 
-        Transaction transaction = session.beginTransaction();
+        //Transaction transaction = session.beginTransaction();
 
         User user = new User();
         user.setEmail(email);
         user.setName(name);
         user.setPassword(password);
-        //em.persist(user);
 
-        session.save(user);
+        try {
 
-        transaction.commit();
+            utx.begin();
+            em.persist(user);
+            utx.commit();
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
+        //session.save(user);
+
+        //transaction.commit();
 
         return true;
     }
