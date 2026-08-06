256d125a9165f3af7dc9f7262203b8211179aedc
RasheedaIdusara <rasheedadusarai590@gmail.com>
2026-08-06 16:11:05 +0530

    programmic timer Practice done and 2026/07/28 projects complete

--- J2EE-Timer/src/main/java/lk/rasheeda/ee/CancelTimer.java
+++ J2EE-Timer/src/main/java/lk/rasheeda/ee/CancelTimer.java
@@ -1,29 +1,27 @@
 package lk.rasheeda.ee;
 
 import jakarta.ejb.EJB;
-import jakarta.ejb.TimerService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lk.rasheeda.ejb.Task;
 import lk.rasheeda.ejb.TimerSessionBean;
 
 import java.io.IOException;
 
-@WebServlet("/timertest")
-public class TestTimer extends HttpServlet {
+@WebServlet("/canceltimer")
+public class CancelTimer extends HttpServlet {
 
     @EJB
     private TimerSessionBean timerSessionBean;
 
-    @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        Task task = timerSessionBean.createTimer();
+        String taskId = req.getParameter("taskId");
 
-        resp.getWriter().write("Task created :" + task.getTaskId());
 
+        timerSessionBean.cancelTimer(taskId);
     }
+
 }
