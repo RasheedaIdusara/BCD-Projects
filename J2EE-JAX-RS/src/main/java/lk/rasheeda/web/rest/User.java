89ddaeeb048914992bb727a3aca9152ae4fb7e44
RasheedaIdusara <rasheedadusarai590@gmail.com>
2026-08-12 08:23:24 +0530

    J2EE-JAX-RS project initizlie and practice CONSUMES AND PRODUCES

--- J2EE-JAX-RS/src/main/java/lk/rasheeda/web/rest/User.java
+++ J2EE-JAX-RS/src/main/java/lk/rasheeda/web/rest/User.java
@@ -1,9 +1,14 @@
 package lk.rasheeda.web.rest;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/user")
+@Consumes({MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_JSON})
 public class User {
 
     @Path("ID")
