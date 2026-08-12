fce8f66f3fe93e2c9d83d172e7b95580307fe521
RasheedaIdusara <rasheedadusarai590@gmail.com>
2026-08-12 08:26:08 +0530

    J2EE-JAX-RS project initizlie and practice CONSUMES AND PRODUCES WITH parms

--- J2EE-JAX-RS/src/main/java/lk/rasheeda/web/rest/User.java
+++ J2EE-JAX-RS/src/main/java/lk/rasheeda/web/rest/User.java
@@ -1,9 +1,6 @@
 package lk.rasheeda.web.rest;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/user")
@@ -13,8 +10,8 @@ public class User {
 
     @Path("ID")
     @GET
-    public String m(){
-        return "ID";
+    public String m(@QueryParam("name") String name) {
+        return "Name: " + name;
     }
 
     @Path("/nic")
