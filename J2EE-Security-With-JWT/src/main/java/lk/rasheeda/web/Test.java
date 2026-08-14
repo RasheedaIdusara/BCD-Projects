76699674ca169da4ab70841144fea4d210e964c2
RasheedaIdusara <rasheedadusarai590@gmail.com>
2026-08-14 22:48:10 +0530

    JWT Token Check Valid or Invalid

--- J2EE-Security-With-JWT/src/main/java/lk/rasheeda/web/Test.java
+++ J2EE-Security-With-JWT/src/main/java/lk/rasheeda/web/Test.java
@@ -7,8 +7,13 @@ import java.util.Set;
 public class Test {
 
     public static void main(String[] args) {
-        String token = JwtUtil.genrateToken("Rasheeda", Set.of("Admin", "User"));
-        System.out.println(token);
+//        String token = JwtUtil.genrateToken("Rasheeda", Set.of("Admin", "User"));
+//        System.out.println(token);
+
+        boolean valid =
+                JwtUtil.isValid("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJSYXNoZWVkYSIsInJvbGVzIjpbIlVzZXIiLCJBZG1pbiJdLCJpYXQiOjE3ODY3Mjc3MzQsImV4cCI6MTc4NjczMTMzNH0.EW8N_LAFsyMj-kEHGqab5EMpjPlvn7haoemYRVybQdI");
+        System.out.println(valid);
+
     }
 
 }
