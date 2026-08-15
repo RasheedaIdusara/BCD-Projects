80fffdc13c415c6b6b6ed28b6d030b154b9af43c
RasheedaIdusara <rasheedadusarai590@gmail.com>
2026-08-15 12:12:22 +0530

    JWT Auth Mechnisam update

--- J2EE-Security-With-JWT/src/main/java/lk/rasheeda/web/Test.java
+++ J2EE-Security-With-JWT/src/main/java/lk/rasheeda/web/Test.java
@@ -7,11 +7,12 @@ import java.util.Set;
 public class Test {
 
     public static void main(String[] args) {
+
 //        String token = JwtUtil.genrateToken("Rasheeda", Set.of("Admin", "User"));
 //        System.out.println(token);
 
         boolean valid =
-                JwtUtil.isValid("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJSYXNoZWVkYSIsInJvbGVzIjpbIlVzZXIiLCJBZG1pbiJdLCJpYXQiOjE3ODY3Mjc3MzQsImV4cCI6MTc4NjczMTMzNH0.EW8N_LAFsyMj-kEHGqab5EMpjPlvn7haoemYRVybQdI");
+                JwtUtil.isValid("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJSYXNoZWVkYSIsInJvbGVzIjpbIkFkbWluIiwiVXNlciJdLCJpYXQiOjE3ODY3MjgwMjksImV4cCI6MTc4NjcyODA1OX0.x_FGkME4mrLxa-Me0sp84eafpGmdLXegnwjOqS93F64");
         System.out.println(valid);
 
     }
--- J2EE-Security-With-JWT/src/main/java/lk/rasheeda/web/security/JWTAuthMechnisam.java
+++ J2EE-Security-With-JWT/src/main/java/lk/rasheeda/web/security/JWTAuthMechnisam.java
@@ -1,5 +1,6 @@
 package lk.rasheeda.web.security;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.security.enterprise.AuthenticationException;
 import jakarta.security.enterprise.AuthenticationStatus;
@@ -7,6 +8,11 @@ import jakarta.security.enterprise.authentication.mechanism.http.HttpAuthenticat
 import jakarta.security.enterprise.authentication.mechanism.http.HttpMessageContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.rasheeda.web.util.JwtUtil;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @ApplicationScoped
 public class JWTAuthMechnisam implements HttpAuthenticationMechanism {
@@ -19,13 +25,27 @@ public class JWTAuthMechnisam implements HttpAuthenticationMechanism {
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
             String token = authHeader.substring(7);
 
             //Validate Token
+            if(JwtUtil.isValid(token)){
+
+                DecodedJWT jwt = JwtUtil.parseToken(token);
+                String username = jwt.getSubject();
+                List<String> rolesList = jwt.getClaim("roles").asList(String.class);
+                Set<String> roles = rolesList != null ? new HashSet<>(rolesList) : Set.of();
+
+                return context.notifyContainerAboutLogin(username, roles);
+
+
+            }
         }
 
+        if (context.isProtected()){
+            return context.responseUnauthorized();
+        }
 
-
-        return null;
+        return context.doNothing();
     }
 }
